25938c0a46c15e708d2aab1046eeeb2370cdcdf2
fred-da0 <fred.da0.me@gmail.com>
2015-03-04 17:12:04 +0000

    HTML to PHP files and moved menu to a php file. Account bar commented

---
+++ web/navMenu.php
@@ -0,0 +1,60 @@
+		<!-- top-header
+		<div class="top-header">
+			<div class="container">
+				<div class="top-header-left">
+					<ul>
+						<li><a href="myaccount.html">My Account</a></li>
+						<li><a href="checkout.html">Checkout</a></li>
+						<div class="clearfix"> </div>
+					</ul>
+				</div>
+				<div class="top-header-center">
+					<p><span class="cart"> </span>Cart<label>$5256</label></p>
+				</div>
+				<div class="top-header-right">
+					<ul>
+						<li>
+							<form>
+								<input type="text">
+								<input type="submit" value="" />
+							</form>
+						</li>
+						<li><a href="login.html">Login</a></li>
+						<li><a href="register.html">Register</a></li>
+					</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<!-- /top-header -->
+		<!-- main-menu -->
+		<div class="main-menu">
+			<div class="container">
+			<div class="head-nav">
+				<span class="menu"> </span>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<!--<li><a href="products.html">products</a></li>-->
+					<!--<li class="active"><a href="about.html">about</a></li>-->
+					<li><a href="contact.php">Contact</a></li>
+					<div class="clearfix"> </div>
+				</ul>
+			</div>	
+				<!-- script-for-nav -->
+					<script>
+						$( "span.menu" ).click(function() {
+						  $( ".head-nav ul" ).slideToggle(300, function() {
+							// Animation complete.
+						  });
+						});
+					</script>
+				<!-- script-for-nav -->
+
+				<!-- logo -->
+				<div class="logo">
+					<a href="index.php"><img src="images/logo.png" title="Sweetcake" /></a>
+				</div>
+				<!-- logo -->
+			</div>
+		</div>
+		<!-- /main-menu -->
---
+++ web/register.php
@@ -0,0 +1,117 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Sweet-cakes Website Template | Home :: w3layouts</title>
+		<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="js/jquery.min.js"></script>
+		 <!-- Custom Theme files -->
+		<link href="css/style.css" rel='stylesheet' type='text/css' />
+   		 <!-- Custom Theme files -->
+   		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		</script>
+		<script src="js/jquery.easydropdown.js"></script>
+		<!----webfonts--->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,700,800,400,600' rel='stylesheet' type='text/css'>
+		<!---//webfonts--->
+	</head>
+	<body>
+	<!-- container -->
+		  <?php include_once 'navMenu.php'; ?>
+	<!---start-content----->
+	<div class="content">
+	<div class="main">
+	   <div class="container">
+		  <div class="register">
+		  	  <form> 
+				 <div class="register-top-grid">
+					<h3>PERSONAL INFORMATION</h3>
+					 <div class="wow fadeInLeft" data-wow-delay="0.4s">
+						<span>First Name<label>*</label></span>
+						<input type="text"> 
+					 </div>
+					 <div class="wow fadeInRight" data-wow-delay="0.4s">
+						<span>Last Name<label>*</label></span>
+						<input type="text"> 
+					 </div>
+					 <div class="wow fadeInRight" data-wow-delay="0.4s">
+						 <span>Email Address<label>*</label></span>
+						 <input type="text"> 
+					 </div>
+					 <div class="clearfix"> </div>
+					   <a class="news-letter" href="#">
+						 <label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i> </i>Sign Up for Newsletter</label>
+					   </a>
+					 </div>
+				     <div class="register-bottom-grid">
+						    <h3>LOGIN INFORMATION</h3>
+							 <div class="wow fadeInLeft" data-wow-delay="0.4s">
+								<span>Password<label>*</label></span>
+								<input type="text">
+							 </div>
+							 <div class="wow fadeInRight" data-wow-delay="0.4s">
+								<span>Confirm Password<label>*</label></span>
+								<input type="text">
+							 </div>
+					 </div>
+				</form>
+				<div class="clearfix"> </div>
+				<div class="register-but">
+				   <form>
+					   <input type="submit" value="submit">
+					   <div class="clearfix"> </div>
+				   </form>
+				</div>
+		   </div>
+	     </div>
+	    </div>
+		</div>
+	<!-- footer -->
+	<div class="footer">
+		<div class="container">
+			<div class="footer-top">
+				<div class="col-md-3 location">
+					<h4>location</h4>
+					<p>#04 Dist.City,State,PK</p>
+					<h4>hours</h4>
+					<p>Weekdays 7 a.m.-7 p.m.</p>
+					<p>Weekends 8 a.m.-7 p.m.</p>
+					<p>Call for Holidays Hours.</p>
+				</div>
+				<div class="col-md-3 customer">
+					<h4>customer service</h4>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod. </p>
+					<h4>phone</h4>
+					<h6>1(234)567-8910</h6>
+					<h4>contact us</h4>
+					<h6>contact us page.</h6>
+				</div>
+				<div class="col-md-3 social">
+					<h4>get social</h4>
+					<div class="face-b">
+						<img src="images/foot.png" title="name"/>
+						<a href="#"><i class="fb"> </i></a>
+					</div>
+					<div class="twet">		
+						<img src="images/foot.png" title="name"/>
+							<a href="#"><i class="twt"> </i></a>
+					</div>	
+				</div>
+				<div class="col-md-3 sign">
+					<h4>sign up for news later</h4>	
+						<form>
+							<input type="text" class="text" value="YOUR EMAIL" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'YOUR EMAIL ';}">
+						</form>
+				</div>
+					<div class="clearfix"> </div>
+			</div>
+			<div class="footer-bottom">
+				<p>Template by <a href="http://w3layouts.com" target="_blank"> w3layouts</a></p>
+			</div>
+		</div>
+	</div>
+	<!-- /footer -->
+	</body>
+</html>
+
