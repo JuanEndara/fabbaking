8fdbd81ed1854ba603e824c937e3b63a01dff6d6
fred-da0 <fred.da0.me@gmail.com>
2015-03-06 12:23:04 +0000

    HTML header changed to file and added FAVICON to all pages

--- web/about.php
+++ web/about.php
@@ -7,19 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Sweet-cakes Website Template | Home :: w3layouts</title>
-		<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="js/jquery.min.js"></script>
-		 <!-- Custom Theme files -->
-		<link href="css/style.css" rel='stylesheet' type='text/css' />
-   		 <!-- Custom Theme files -->
-   		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-		</script>
-		<!----webfonts--->
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,700,800,400,600' rel='stylesheet' type='text/css'>
-		<!---//webfonts--->
+        <?php include_once 'htmlHeader.php'; ?>
 	</head>
 	<body>
 	<!-- container -->
